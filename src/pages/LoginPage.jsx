56ef6d3ddd64f911b5ec55b9a6735573a0426613
yasaman rezaeeshirsavar <yasaman@dineeasy.ca>
2021-06-16 20:27:02 +0430

    add apicall axios

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { login } from "./../api/login";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const handleLogin = () => {
-    localStorage.setItem(
-      "token",
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    );
-    window.location.reload();
+    login(email, password)
+      .then((res) => {
+        localStorage.setItem("token", res.data.token);
+        window.location.reload();
+      })
+      .catch((err) => console.error(err));
   };
   return <button onClick={handleLogin}>Login</button>;
 };
